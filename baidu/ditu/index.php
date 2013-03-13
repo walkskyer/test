52df751b5fea31f3c94086b38b70e2da422aa2c5
weijie <walkskyer@88jianshen.com>
2013-03-13 14:22:55 +0800

    s

--- baidu/ditu/index.php
+++ baidu/ditu/index.php
@@ -48,7 +48,7 @@ if(!empty($_POST)){
 <script type="text/javascript">
     var map = new BMap.Map("container"); // 创建地图实例
     var point = new BMap.Point(117.003, 36.655); // 创建点坐标
-    map.centerAndZoom(point, 15); // 初始化地图，设置中心点坐标和地图级别
+    map.centerAndZoom(point, 13); // 初始化地图，设置中心点坐标和地图级别
 
     map.addControl(new BMap.NavigationControl());
     map.addControl(new BMap.ScaleControl());
