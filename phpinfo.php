4345bd30a1a3fa7080f94adc57cb9c64abee36e9
weijie <walkskyer@88jianshen.com>
2013-03-15 17:34:51 +0800

    增加一个输出phpinfo的文件。

---
+++ phpinfo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: weijie
+ * Date: 13-3-15
+ * Time: 下午5:20
+ * File: phpinfo.php
+ * To change this template use File | Settings | File Templates.
+ */
+phpinfo();
