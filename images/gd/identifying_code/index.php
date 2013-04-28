046778c92c17a55802d7beb4f81b233e84d58c35
weijie <walkskyer@88jianshen.com>
2013-04-28 09:15:16 +0800

    GD生成验证码的实例。

---
+++ images/gd/identifying_code/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: weijie
+ * Date: 13-4-28
+ * Time: 上午9:07
+ * File: index.php
+ * To change this template use File | Settings | File Templates.
+ */?>
+<html>
+<head>
+    <title>验证码测试</title>
+</head>
+<body>
+<img src="/images/gd/identifying_code/identifying_code.php?<?php echo time();?>" />
+</body>
