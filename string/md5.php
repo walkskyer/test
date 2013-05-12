8c9a85306305719c68c6e4e1cd01a5626509fc7a
walkskyer <walkskyer@qq.com>
2013-05-12 12:25:07 +0800

    测试md5函数

---
+++ string/md5.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: adminer
+ * Date: 13-5-12
+ * Time: 上午10:52
+ * To change this template use File | Settings | File Templates.
+ */
+$src = 'this is a test.';
+$md5=md5($src);
+echo $md5;
