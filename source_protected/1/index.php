dd138787e6be9c581f950af2967853223483fc41
weijie <walkskyer@88jianshen.com>
2013-05-13 16:43:46 +0800

    添加一个可用于加密php源代码的程序

---
+++ source_protected/1/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: weijie
+ * Date: 13-5-13
+ * Time: 下午4:17
+ * File: index.php
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'hello world!';
