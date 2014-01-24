395745e397cd587513f6139116f80bbeb2e16fb9
walkskyer <walkskyer@qq.com>
2014-01-24 22:18:06 +0800

    添加测试客户端请求类型的脚本。

---
+++ request/request.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: adminer
+ * Date: 14-1-24
+ * Time: 下午9:29
+ * To change this template use File | Settings | File Templates.
+ */
+if(!empty($_GET)){
+    echo 'get is not empty';
+}
+if(!empty($_POST)){
+    echo 'post is not empty';
+}
