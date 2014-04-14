f07f3ac996bc36b61c49a0ab50808e3a63882c77
walkskyer <walkskyer@qq.com>
2014-04-14 16:39:35 +0800

    添加一个测试discuz authcode函数的程序。

---
+++ discuz/test_authcode.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walkskyer
+ * Date: 14-4-14
+ * Time: 下午3:56
+ */
+define('AUTHKEY',md5('walkskyer'));
+echo AUTHKEY.'<br/>';
+$string ='zwj';
+echo 'string:'.$string;
+echo '<br/>';
+echo 'base64_encode:'.base64_encode($string);echo '<br/>';
+echo 'base64_decode:'.base64_decode(base64_encode($string));echo '<br/>';
+echo 'authcode:'.authcode($string,'ENCODE');echo '<br/>';
+function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+    $ckey_length = 4;
+    $key = md5($key != '' ? $key : AUTHKEY);
+    $keya = md5(substr($key, 0, 16));
+    $keyb = md5(substr($key, 16, 16));
+    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
+
+    $cryptkey = $keya.md5($keya.$keyc);
+    $key_length = strlen($cryptkey);
+
+    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+    $string_length = strlen($string);
+
+    $result = '';
+    $box = range(0, 255);
+
+    $rndkey = array();
+    for($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+
+    for($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+
+    for($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+
+    if($operation == 'DECODE') {
+        if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+            return substr($result, 26);
+        } else {
+            return '';
+        }
+    } else {
+        echo 'keyc:'.$keyc.'<br/>';
+        echo 'result:'.$result.'<br/>';
+        echo 'keyc.result:'.$keyc.$result.'<br/>';
+        return $keyc.str_replace('=', '', base64_encode($result));
+    }
+
+}
+
+// 参数解释
+// $string： 明文 或 密文
+// $operation：DECODE示意 解密,其它示意 加密
+// $key： 密匙
+// $exp iry：密文有效期
+function authcode_jieshi($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+    // 动态密匙长度，相似 的明文会生成不一样 密文就是依靠动态密匙
+    $ckey_length = 4;
+
+    // 密匙
+    $key = md5($key ? $key : $GLOBALS['discuz_auth_key']);
+
+    // 密匙a会参与加解密
+    $keya = md5(substr($key, 0, 16));
+    // 密匙b会用来做数据完整性验证
+    $keyb = md5(substr($key, 16, 16));
+    // 密匙c用于改动 生成的密文
+    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length) :
+        substr(md5(microtime()), -$ckey_length)) : '';
+    // 参与运算的密匙
+    $cryptkey = $keya . md5($keya . $keyc);
+    $key_length = strlen($cryptkey);
+    // 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存$keyb(密匙b)，解密时会议决 这个密匙验证数据完整性
+    // 假如 是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确
+    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) :
+        sprintf('%010d', $expiry ? $expiry + time() : 0) . substr(md5($string . $keyb), 0, 16) . $string;
+    $string_length = strlen($string);
+    $result = '';
+    $box = range(0, 255);
+    $rndkey = array();
+    // 产生密匙簿
+    for ($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+    // 用固定的算法，打乱密匙簿，添加 随机性，好像很庞杂 ，实际上对并不会添加 密文的强度
+    for ($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+    // 核心加解密部分
+    for ($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        // 从密匙簿得出密匙实行 异或，再转成字符
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+    if ($operation == 'DECODE') {
+        // substr($result, 0, 10) == 0 验证数据有效性
+        // substr($result, 0, 10) - time() > 0 验证数据有效性
+        // substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16) 验证数据完整性
+        // 验证数据有效性，请看未加密明文的格式
+        if ((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26) . $keyb), 0, 16)) {
+            return substr($result, 26);
+        } else {
+            return '';
+        }
+    } else {
+        // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不一样 密文后能解密的原由
+        // 因为加密后的密文可能是一些特殊字符，复制流程 可能会丢失，所以用base64编码
+        return $keyc . str_replace('=', '', base64_encode($result));
+    }
+}
