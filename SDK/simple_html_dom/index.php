1e7cfaf995ec1a7362bef9050e17de5b673a6ec8
walkskyer <walkskyer@qq.com>
2014-04-24 14:28:06 +0800

    添加测试simple_html_dom.php处理时间的脚本。

--- SDK/simple_html_dom/index.php
+++ SDK/simple_html_dom/index.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+    <title>simple html dom 测试</title>
+</head>
+<body>
+<form method="post">
+    <textarea name="url"><?php echo isset($_POST['url'])?$_POST['url']:'http://www.baidu.com';?></textarea>
+    <input type="submit">
+</form>
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,8 +16,14 @@
  * Date: 14-4-23
  * Time: 下午5:49
  */
+if(!isset($_POST['url'])) exit;
 require_once('simple_html_dom.php');
-$html = file_get_html('http://www.baidu.com/');
+$starttime =microtime_float();
+$html = file_get_html($_POST['url']);
+$endtime = microtime_float();
+print $starttime.'<br>';
+print $endtime.'<br>';
+print $endtime-$starttime.'<br>';
 
 // Find all images
 foreach($html->find('img') as $element)
@@ -15,3 +32,9 @@ echo "--------------------------------------------------------------------------
 // Find all links
 foreach($html->find('a') as $element)
     echo $element->href . '<br>';
+
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
