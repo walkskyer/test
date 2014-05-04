91b41d4ef8302cce9c244698f01a66784939af95
walkskyer <walkskyer@qq.com>
2014-05-04 19:41:27 +0800

    添加一个图片展模板。

---
+++ html/template/520mmm/templates/520mmm/js/fun.js
@@ -0,0 +1,720 @@
+/**
+ * @author rxw
+ * 整站广告位
+ */
+
+
+/********************首页广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_index_01()
+{
+
+}
+
+/**
+ * 960 X *
+ */
+function show_index_02()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_index_03()
+{
+	
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106335;");
+document.writeln("var sogou_ad_height=90;");
+document.writeln("var sogou_ad_width=960;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/c.js\'><\/script>");
+
+
+}
+
+/**
+ * 960 X *
+ */
+function show_index_04()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>")
+}
+
+/**
+ * 960 X *
+ */
+function show_index_05()
+{
+	
+
+
+}
+
+
+/**
+ * 960 X *
+ */
+function show_index_06()
+{
+
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>")
+
+}
+
+/**
+ * 960 X *
+ */
+function show_index_07()
+{
+	
+
+}
+/**
+ * 960 X *
+ */
+function show_index_08()
+{
+	
+}
+
+/**
+ * 300 X *
+ */
+function show_index_right_fudong()
+{
+	//document.writeln("<iframe width=\"280\" scrolling=\"no\" height=\"295\" frameborder=\"0\" src=\"http:\/\/www.xingzuo.com\/hezuo\/xingzuo_300X350.html\"><\/iframe>");
+}
+
+
+/**
+ * 728 X *
+ */
+function show_index_h2_01()
+{
+	
+	
+}
+
+function show_index_h2_02()
+{
+	
+	
+}
+
+function show_index_h2_03()
+{
+
+
+}
+
+function show_index_h2_04()
+{
+
+
+}
+
+/********************最新美女广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_new_01()
+{
+	
+
+}
+
+/**
+ * 960 X *
+ */
+function show_new_02()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+
+/**
+ * 960 X *
+ */
+function show_new_03()
+{
+	
+
+
+}
+/**
+ * 500 X *
+ */
+function show_new_h1_01()
+{
+	
+
+}
+/********************最新更新广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_new_update_01()
+{
+	
+
+}
+
+/**
+ * 960 X *
+ */
+function show_new_update_02()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+/**
+ * 960 X *
+ */
+function show_new_update_03()
+{
+
+
+
+}
+
+/**
+ * 500 X *
+ */
+function show_new_update_h1_01()
+{
+	
+
+}
+
+/********************列表广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_list_01()
+{
+
+}
+
+/**
+ * 960 X *
+ */
+function show_list_02()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_list_03()
+{
+	
+
+}
+
+/**
+ * 250 X *
+ */
+function show_list_04()
+{
+	
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106312;");
+document.writeln("var sogou_ad_height=250;");
+document.writeln("var sogou_ad_width=250;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/c.js\'><\/script>");
+
+}
+
+/**
+ * 250 X *
+ */
+function show_list_05()
+{
+	
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_250*250，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255102\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+
+/**
+ * 250 X *
+ */
+function show_list_06()
+{
+	
+
+}
+
+/**
+ * 250 X *
+ */
+function show_list_07()
+{
+	document.writeln("<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\">");
+document.writeln("        <tbody>");
+document.writeln("        	<tr>");
+document.writeln("          <td height=\"10\" colspan=\"2\"><\/td>");
+document.writeln("		 <\/tr>");
+document.writeln("        <tr>");
+document.writeln("          <td align=\"center\" style=\"PADDING-LEFT: 3px; PADDING-BOTTOM: 0px; LINE-HEIGHT: 22px; BORDER-BOTTOM: #e3e3e3 1px dotted\">");
+document.writeln("		  	<a target=\"_blank\" href=\"http:\/\/www.520mmm.net\/xingganmeinv\/45\/8953_1.html\">");
+document.writeln("		  		<img width=\"100\" style=\"border: 1px solid #CCCCCC;\" border=\"0\" src=\"http:\/\/pic.520mmm.net:7588\/520upload\/articleDownloadImg\/20111212\/s_13236208389916223456781.jpg.jpg\">");
+document.writeln("			<\/a>");
+document.writeln("		 <\/td>");
+document.writeln("          <td align=\"left\" valign=\"top\" style=\"PADDING-LEFT: 5px; LINE-HEIGHT: 22px; BORDER-BOTTOM: #e3e3e3 1px dotted\">");
+document.writeln("		  <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/46\/9026_1.html\" target=\"_blank\"><font color=\"#ff0000\">超级美胸极品美女<\/font><\/a><br>");
+document.writeln("		  <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/46\/9018_1.html\" target=\"_blank\">蛇美女缠绕大胆火辣<\/a><br>");
+document.writeln("		  <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/45\/8991_1.html\" target=\"_blank\"><font color=\"#ff3300\">2011最性感的女神<\/font><\/a><br>");
+document.writeln("		  <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/45\/8872_1.html\" target=\"_blank\">原干惠高清写真现美胸<\/a><br>");
+document.writeln("		  <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/45\/8989_1.html\" target=\"_blank\">风俗娘2011合集套图<\/a>");
+document.writeln("		 <\/td><\/tr>");
+document.writeln("        <tr align=\"center\">");
+document.writeln("          <td height=\"10\" colspan=\"2\"><\/td><\/tr>");
+document.writeln("        <tr>");
+document.writeln("          <td align=\"center\" style=\"PADDING-LEFT: 3px; PADDING-BOTTOM: 0px; LINE-HEIGHT: 22px\">");
+document.writeln("		  <a target=\"_blank\" href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/45\/8989_1.html\">");
+document.writeln("		  	<img width=\"100\" style=\"border: 1px solid #CCCCCC;\" border=\"0\" src=\"http:\/\/pic.520mmm.net:7588\/520upload\/articleDownloadImg\/20111221\/s_13244689382928421343501.jpg.jpg\">");
+document.writeln("			<\/a>");
+document.writeln("		<\/td>");
+document.writeln("          <td align=\"left\" valign=\"top\" style=\"PADDING-LEFT: 3px; PADDING-BOTTOM: 0px; LINE-HEIGHT: 22px\">");
+document.writeln("		  <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/46\/9054_1.html\" target=\"_blank\"><font color=\"#ff3300\">素心韩国风俗娘系列套图<\/font><\/a><br>");
+document.writeln("            <a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/46\/9042_1.html\" target=\"_blank\">刘亦菲高清大图大杂汇<\/a><br>");
+document.writeln("			<a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/45\/8991_1.html\" target=\"_blank\"><font color=\"#ff3300\">2011精选最性感的女神<\/font><\/a><br>");
+document.writeln("			<a href=\"http:\/\/www.520mmm.net\/jinghuatuijian/45\/8944_1.html\" target=\"_blank\">日本辣妹诱惑写真<\/a><br>");
+document.writeln("			<a href=\"http:\/\/www.520mmm.net\/jinghuatuijian\/45\/8833_1.html\" target=\"_blank\">侯诗辰VIP诊室制服诱惑<\/a>");
+document.writeln("		<\/td>");
+document.writeln("        <\/tr>");
+document.writeln("		<tr>");
+document.writeln("          <td height=\"5\" colspan=\"2\"><\/td>");
+document.writeln("		 <\/tr>");
+document.writeln("		  <\/tbody>");
+document.writeln("	<\/table>");
+}
+
+
+/**
+ * 250 X *
+ */
+function show_list_08()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_250*250，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255102\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+/**
+ * 500 X *
+ */
+function show_list_h1_01()
+{
+	
+
+}
+
+
+/********************搜索广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_search_01()
+{
+	
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+/**
+ * 960 X *
+ */
+function show_search_02()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106335;");
+document.writeln("var sogou_ad_height=90;");
+document.writeln("var sogou_ad_width=960;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/c.js\'><\/script>");
+
+
+}
+
+/**
+ * 960 X *
+ */
+function show_search_03()
+{
+
+}
+
+
+/**
+ * 728 X *
+ */
+function show_search_h1_01()
+{
+	
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106332;");
+document.writeln("var sogou_ad_height=20;");
+document.writeln("var sogou_ad_width=728;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/l.js\'><\/script>");
+
+
+}
+/*******************专题广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_topics_01()
+{
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_topics_02()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_topics_03()
+{
+	
+}
+
+
+/**
+ * 960 X *
+ */
+function show_topics_04()
+{
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_topics_05()
+{
+	
+
+
+}
+
+/**
+ * 300 X *
+ */
+function show_topics_06()
+{
+	
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_300*250，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255111\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+
+/**
+ * 960 X *
+ */
+function show_topics_07()
+{
+	
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+
+/**
+ * 728 X *
+ */
+function show_topics_h2_01()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106332;");
+document.writeln("var sogou_ad_height=20;");
+document.writeln("var sogou_ad_width=728;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/l.js\'><\/script>");
+
+	
+}
+
+/*******************详细广告位******************/
+
+/**
+ * 960 X *
+ */
+function show_view_01()
+{
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_960*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255082\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+/**
+ * 960 X *
+ */
+function show_view_02()
+{
+	
+
+}
+
+/**
+ * 960 X *
+ * 
+ * 
+
+ */
+function show_view_03()
+{
+	document.writeln("<style>");
+	document.writeln('#BAIDU_DUP_wrapper_u1255111_0{ width:300px; 250px; margin:10px 0 10px 200px; padding:0; float:left;}');
+	document.writeln('#BAIDU_DUP_wrapper_u1255111_1{ width:300px; 250px; margin:10px 10px; padding:0; float:left;}');
+	document.writeln("</style>");
+	
+	
+	document.writeln("<br />");
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_300*250，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255111\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+	
+	document.writeln("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+	
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_300*250，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255111\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+	
+
+
+}
+
+
+/**
+ * 960 X *
+ */
+function show_view_04()
+{
+
+document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew 960*30，创建于2014-2-8*\/");
+document.writeln("var cpro_id = \"u1465707\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+document.writeln("<div class=\"hb10\"><\/div>");
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_view_05()
+{
+	//内容交换
+	document.writeln("<iframe scrolling=\"no\" height=\"95\" width=\"960\" frameborder=\"0\" align=\"center,center\" src=\"\/xg\/99_960_90.html\" allowtransparency=\"\" marginheight=\"0\" marginwidth=\"0\" id=\"jh\"><\/iframe>");
+	
+	//document.writeln("<script type=\"text\/javascript\">\/*520mmm内2 960*90，创建于2012-5-31*\/ var cpro_id = \'u921232\';<\/script><script src=\"http:\/\/cpro.baidu.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+	
+}
+/**
+ * 960 X *
+ */
+function show_view_06()
+{
+	
+
+}
+/**
+ * 960 X *
+ */
+function show_view_07()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106304;");
+document.writeln("var sogou_ad_height=90;");
+document.writeln("var sogou_ad_width=960;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/c.js\'><\/script>");
+
+
+}
+
+
+/**
+ * 960 X *
+ */
+function show_view_08()
+{
+	
+
+}
+
+/**
+ * 960 X *
+ */
+function show_view_09()
+{
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_view_10()
+{
+		
+	
+}
+
+/**
+ * 960 X *
+ */
+function show_view_11()
+{
+	
+
+}
+
+
+/********************meinv.html**********************/
+
+/**
+ * 728 X *
+ */
+function jh_ads_01()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("\/*520mmmnew_728*90，创建于2013-4-8*\/");
+document.writeln("var cpro_id = \"u1255120\";");
+document.writeln("<\/script>");
+document.writeln("<script src=\"http:\/\/cpro.baidustatic.com\/cpro\/ui\/c.js\" type=\"text\/javascript\"><\/script>");
+
+}
+
+/**
+ * 960 X *
+ */
+function jh_ads_02()
+{
+	
+
+}
+
+/**
+ * 960 X *
+ */
+function jh_ads_h1_01()
+{
+	document.writeln("<script type=\"text\/javascript\">");
+document.writeln("var sogou_ad_id=106332;");
+document.writeln("var sogou_ad_height=20;");
+document.writeln("var sogou_ad_width=728;");
+document.writeln("<\/script>");
+document.writeln("<script language=\'JavaScript\' type=\'text\/javascript\' src=\'http:\/\/images.sohu.com\/cs\/jsfile\/js\/l.js\'><\/script>");
+
+}
+
+/**
+ * 960 X *
+ */
+function jh_ads_03()
+{
+	
+}
+
+/**
+ * 960 X *
+ */
+function jh_ads_04()
+{
+	
+	document.writeln("<iframe id=\"showad\" name=\"showad\" src=\"http:\/\/www.xplian.com\/c\/showpic.html?un=xx&fontsize=12&bordercolor=dddddd&bgcolor=ffffff&titlecolor=000000&hcolor=FF0000&hbgcolor=ffffff&underline=2&inline=1&juzhong=1&height=215px&width=100%&hang=1&lie=6&picwidth=0&picheight=0&pics=6\" frameborder=\"0\" scrolling=\"no\" style=\"width:100%; height:215px;\"><\/iframe>");
+	
+	
+}
+
+/**
+ * 960 X *
+ */
+function jh_ads_05()
+{
+	//弹窗
+	//document.write('<script src="http://js.cy123.cc:99/page/?s=12100"></script>');
+	
+}
---
+++ html/template/520mmm/templates/520mmm/js/guandong.js
@@ -0,0 +1,202 @@
+function $(id) { return document.getElementById(id); }
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+function moveElement(elementID,final_x,final_y,interval) {
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem = document.getElementById(elementID);
+  if (elem.movement) {
+    clearTimeout(elem.movement);
+  }
+  if (!elem.style.left) {
+    elem.style.left = "0px";
+  }
+  if (!elem.style.top) {
+    elem.style.top = "0px";
+  }
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  if (xpos == final_x && ypos == final_y) {
+		return true;
+  }
+  if (xpos < final_x) {
+    var dist = Math.ceil((final_x - xpos)/10);
+    xpos = xpos + dist;
+  }
+  if (xpos > final_x) {
+    var dist = Math.ceil((xpos - final_x)/10);
+    xpos = xpos - dist;
+  }
+  if (ypos < final_y) {
+    var dist = Math.ceil((final_y - ypos)/10);
+    ypos = ypos + dist;
+  }
+  if (ypos > final_y) {
+    var dist = Math.ceil((ypos - final_y)/10);
+    ypos = ypos - dist;
+  }
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+  elem.movement = setTimeout(repeat,interval);
+}
+
+function classNormal(iFocusBtnID,iFocusTxID){
+	var iFocusBtns= $(iFocusBtnID).getElementsByTagName('li');
+	var iFocusTxs = $(iFocusTxID).getElementsByTagName('li');
+	for(var i=0; i<iFocusBtns.length; i++) {
+		iFocusBtns[i].className='normal';
+		iFocusTxs[i].className='normal';
+	}
+}
+
+function classCurrent(iFocusBtnID,iFocusTxID,n){
+	var iFocusBtns= $(iFocusBtnID).getElementsByTagName('li');
+	var iFocusTxs = $(iFocusTxID).getElementsByTagName('li');
+	iFocusBtns[n].className='current';
+	iFocusTxs[n].className='current';
+}
+
+function iFocusChange() {
+	if(!$('ifocus')) return false;
+	$('ifocus').onmouseover = function(){atuokey = true};
+	$('ifocus').onmouseout = function(){atuokey = false};
+	var iFocusBtns = $('ifocus_btn').getElementsByTagName('li');
+	var listLength = iFocusBtns.length;
+	iFocusBtns[0].onmouseover = function() {
+		moveElement('ifocus_piclist',0,0,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',0);
+	}
+	if (listLength>=2) {
+		iFocusBtns[1].onmouseover = function() {
+			moveElement('ifocus_piclist',0,-319,5);
+			classNormal('ifocus_btn','ifocus_tx');
+			classCurrent('ifocus_btn','ifocus_tx',1);
+		}
+	}
+	if (listLength>=3) {
+		iFocusBtns[2].onmouseover = function() {
+			moveElement('ifocus_piclist',0,-638,5);
+			classNormal('ifocus_btn','ifocus_tx');
+			classCurrent('ifocus_btn','ifocus_tx',2);
+		}
+	}
+	if (listLength>=4) {
+		iFocusBtns[3].onmouseover = function() {
+			moveElement('ifocus_piclist',0,-957,5);
+			classNormal('ifocus_btn','ifocus_tx');
+			classCurrent('ifocus_btn','ifocus_tx',3);
+		}
+	}
+}
+
+setInterval('autoiFocus()',5000);
+var atuokey = false;
+function autoiFocus() {
+	if(!$('ifocus')) return false;
+	if(atuokey) return false;
+	var focusBtnList = $('ifocus_btn').getElementsByTagName('li');
+	var listLength = focusBtnList.length;
+	for(var i=0; i<listLength; i++) {
+		if (focusBtnList[i].className == 'current') var currentNum = i;
+	}
+	if (currentNum==0&&listLength!=1 ){
+		moveElement('ifocus_piclist',0,-319,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',1);
+	}
+	if (currentNum==1&&listLength!=2 ){
+		moveElement('ifocus_piclist',0,-638,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',2);
+	}
+	if (currentNum==2&&listLength!=3 ){
+		moveElement('ifocus_piclist',0,-957,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',3);
+	}
+	if (currentNum==3 ){
+		moveElement('ifocus_piclist',0,0,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',0);
+	}
+	if (currentNum==1&&listLength==2 ){
+		moveElement('ifocus_piclist',0,0,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',0);
+	}
+	if (currentNum==2&&listLength==3 ){
+		moveElement('ifocus_piclist',0,0,5);
+		classNormal('ifocus_btn','ifocus_tx');
+		classCurrent('ifocus_btn','ifocus_tx',0);
+	}
+}
+addLoadEvent(iFocusChange);
+
+
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+ 
+/*
+function scrollDoor(){
+}
+scrollDoor.prototype = {
+    sd : function(menus,divs,openClass,closeClass){
+        var _this = this;
+        if(menus.length != divs.length)
+        {
+            alert("菜单层数量和内容层数量不一样!");
+            return false;
+        }
+        for(var i = 0 ; i < menus.length ; i++)
+        {
+            _this.$(menus[i]).value = i;
+            _this.$(menus[i]).onmouseover = function(){
+ 
+                for(var j = 0 ; j < menus.length ; j++)
+                {
+                    _this.$(menus[j]).className = closeClass;
+                    _this.$(divs[j]).style.display = "none";
+                }
+                _this.$(menus[this.value]).className = openClass;
+                _this.$(divs[this.value]).style.display = "block";
+            }
+        }
+        },
+    $ : function(oid){
+        if(typeof(oid) == "string")
+        return document.getElementById(oid);
+        return oid;
+    }
+}
+addLoadEvent(function(){
+    var SDmodel = new scrollDoor();
+    SDmodel.sd(["m01","m02"],["c01","c02"],"a","b");
+
+function killErrors() {
+    return true;
+}
+window.onerror = killErrors;
+});*/
